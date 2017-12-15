4e8d3a5db493851fc58ecbc70ffd6e8bf79e48ae
chesseswar <chesseswar@gmail.com>
2017-12-15 16:27:43 -0600

    input

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@ import java.io.*;
 
 public class Main {
     public static void main (String[] args) throws IOException {
-        Scanner in = new Scanner();
+        PrintWriter writer = new PrintWriter(new FileWriter("output.csv"));
+        Scanner in = new Scanner(new File("Large Data Set.txt"));
+        ArrayList<Double> loadFactors = new ArrayList<>();
+        for (double i = .1; i <= 1; i += .1){
+            loadFactors.add(i);
+        }
+
+
     }
 }
